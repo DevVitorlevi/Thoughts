8d2236ae23ee15d6b75d1e3e82eaac5bf4c2169b
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-03 12:51:09 -0300

    add

--- db/conn.js
+++ db/conn.js
@@ -1,3 +1,19 @@
-const {Sequelize} = require('sequelize')
+// Importa o módulo Sequelize para interagir com o banco de dados.
+const { Sequelize } = require('sequelize')
 
-const sequelize = 
+// Cria uma instância de conexão com o banco de dados.
+const sequelize = new Sequelize('tougths', 'root', '', { // Define os parâmetros de conexão.
+    host: 'localhost', // O endereço do servidor do banco de dados (neste caso, local).
+    port: 3306, // A porta padrão do MySQL.
+    dialect: 'mysql' // Especifica o tipo de banco de dados usado.
+})
+
+// Testa a conexão com o banco de dados.
+try {
+    console.log('Conexão Feita') // Mensagem de sucesso caso a conexão seja estabelecida.
+} catch (err) {
+    console.log('erro ao conectar ao banco de dados') // Mensagem de erro caso a conexão falhe.
+}
+
+// Exporta a instância do Sequelize para ser usada em outros arquivos.
+module.exports = sequelize
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const conn = require('./db/conn');
-const session = require('express-session')
+const session = require('express-session');
+const flash = require('express-flash');
 const FileStore = require('session-file-store')(session)
 
 const app = express();
@@ -14,8 +15,33 @@ app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(session({
+    name:'session',
+    secret:'nosso_secret',
+    resave:false,
+    saveUninitialized:false,
+    store:new FileStore({
+        logFn:function (){},
+        path:require('path').join(require('os').tmpdir(),'sessions'),
+    }),
+    cookie:{
+        secure:false,
+        maxAge:360000,
+        expires:new Date(Date.now() + 360000),
+        httpOnly:true
+    }
+}))
+
+app.use(flash())
 // Middleware para servir arquivos estáticos
 app.use(express.static('public'));
+
+app.use((req,res,next)=>{
+    if(req.session.userid){
+        res.locals.session =req.session
+}
+    next()
+})
 
 // Rotas
 app.use('/task', TaskRoutes);
