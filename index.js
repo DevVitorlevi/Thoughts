a0f76614c60a0d39c66cfb61d148e6ceb9e0b4d7
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-03 12:28:56 -0300

    Modulos e MVC feito

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const conn = require('./db/conn');
+
+const app = express();
+
+// Configuração do Handlebars
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+// Middleware para processar dados do corpo da requisição
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware para servir arquivos estáticos
+app.use(express.static('public'));
+
+// Rotas
+app.use('/task', TaskRoutes);
+    conn.sync().then(()=>app.listen(3000)).catch((e=>console.log(e)))
