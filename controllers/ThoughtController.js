972077f14e688a04d38f6e1cab98c887fe351311
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-06 13:23:20 -0300

    att

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -4,6 +4,6 @@ const Tougth = require('../models/Thought')
 module.exports = class ThoughtController {
     static async allThought(req,res){
         
-        res.render('thougths/home')
+        res.render('thoughts/home')
     }
 }
