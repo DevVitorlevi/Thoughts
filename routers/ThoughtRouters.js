52adfd4a8dd242a80262b853dafafa7f9a98f4a0
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-09 11:37:34 -0300

    feita a dashboard

--- routers/ThoughtRouters.js
+++ routers/ThoughtRouters.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const ThoughtController = require('../controllers/ThoughtController')
+const checkAuth = require('../helpers/checkauth').checkAuth
 
-router.get('/', ThoughtController.allThought)
+router.get('/',ThoughtController.allThought)
+router.get('/dashboard',checkAuth,ThoughtController.dashboard)
 
 
 module.exports = router
